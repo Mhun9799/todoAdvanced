7c14693c839609b0e3d107579f4aac724b83a3f6
Mhun9799 <muhun1030@gmail.com>
2024-01-18 11:23:59 +0900

    Initial commit

---
+++ src/test/kotlin/com/teamsparta/todoadvanced/TodoAdvancedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.todoadvanced
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoAdvancedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
