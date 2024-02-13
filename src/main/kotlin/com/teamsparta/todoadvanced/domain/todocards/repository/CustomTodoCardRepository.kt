1153e25d6ff6a70a0327d63808450e198d67f336
Mhun9799 <muhun1030@gmail.com>
2024-02-13 18:04:43 +0900

    feature: 페이징, QueryDsl 추가수정

--- src/main/kotlin/com/teamsparta/todoadvanced/domain/todocards/repository/CustomTodoCardRepository.kt
+++ src/main/kotlin/com/teamsparta/todoadvanced/domain/todocards/repository/CustomTodoCardRepository.kt
@@ -1,9 +1,15 @@
 package com.teamsparta.todoadvanced.domain.todocards.repository
 
+import com.querydsl.core.BooleanBuilder
 import com.teamsparta.todoadvanced.domain.todocards.model.TodoCard
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
 interface CustomTodoCardRepository {
     fun findAllByOrderByCreatedAtDesc(): List<TodoCard>
     fun findAllByOrderByCreatedAtAsc(): List<TodoCard>
     fun findAllByAuthorId(authorId: Long): List<TodoCard>
+
+    fun findByPageableAndStatus(pageable: Pageable,title: String?): Page<TodoCard>
+
 }
